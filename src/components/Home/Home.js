14296a64f8712def5653b64e569207513b94f789
michaelroudnitski <michaelroudnitski@gmail.com>
2018-11-13 05:06:04 -0500

    small changes and polishing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -25,17 +25,17 @@ export default class Home extends React.Component {
                     shadow: {
                       enable: true,
                       color: "rgb(255, 87, 51)",
-                      blur: 5
+                      blur: 10
                     }
                   }
                 }
               }}
               style={{
                 width: '100%',
-                'z-index': '-1',
+                zIndex: '-1',
                 position: 'absolute'
               }}
-            >Eat Ass</Particles>
+            />
             <div id="name-container">
               <div>
                 <h1 className="pre-name">Hi, my name is</h1>
@@ -59,17 +59,16 @@ export default class Home extends React.Component {
               <div className="box box-2 col">
                 {/* <FontAwesomeIcon icon={faUser} className="faded-background-icon" /> */}
                 <Link to="/about">
-                  <h1 className="row justify-content-center">About Me</h1>
+                  <h1 className="row justify-content-center colored-link">About Me</h1>
                 </Link>
                 <br />
-                <p className="row justify-content-center">
+                <p className="row justify-content-md-center">
                   I'm in my 2nd year of Computer Science at York University in Toronto, Canada.
                 </p>
-                <p className="row justify-content-center inline">
+                <p className="row justify-content-md-center inline">
                   When I'm not studying I'm building beautiful and easy to use web interfaces
                   as well as scalable back end cloud solutions for{' '}
-                  <a href="https://www.completelymanaged.com/" className="inline colored">Completely Managed</a>
-                  {' '}as an intern.
+                  <a href="https://www.completelymanaged.com/" className="inline colored">Completely Managed</a>.
                 </p>
               </div>
             </Fade>
@@ -77,7 +76,7 @@ export default class Home extends React.Component {
               <div className="box box-3 col">
                 {/* <FontAwesomeIcon icon={faCode} className="faded-background-icon" /> */}
                 <Link to="/projects">
-                  <h1 className="row justify-content-center">Projects</h1>
+                  <h1 className="row justify-content-center colored-link">Projects</h1>
                 </Link>
               </div>
             </Fade>
