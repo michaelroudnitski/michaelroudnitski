d5e70022c0248994c43e3aae8d93c2a9043c08df
michaelroudnitski <michaelroudnitski@gmail.com>
2018-11-13 16:55:49 -0500

    skills box

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,10 +7,22 @@ import mountains from "../../../img/mountains.jpg";
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin, faJs, faJava, faPython, faHtml5, faCss3, faNode, faGit, faAndroid, } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope, faDatabase } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faJs, faJava, faPython, faHtml5, faCss3, faNode, faGit, faAndroid, faAngular } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { skill: "Skills" };
+
+    this.highlightSkill = this.highlightSkill.bind(this);
+  }
+
+  highlightSkill(skill) {
+    this.setState(state => ({ skill }))
+  }
+
   render() {
     return (
       <div className="container-fluid">
@@ -55,39 +67,39 @@ export default class Home extends React.Component {
               </div>
             </Fade>
             <Fade>
-              <div className="box box-3 col">
-                <h1 className="row justify-content-center">Skills</h1>
+              <div className="box box-3 col" onMouseLeave={() => this.highlightSkill("Skills")}>
+                <h1 className="row justify-content-center">{this.state.skill}</h1>
                 <div className="row align-items-center">
                   <div className="col">
-                    <FontAwesomeIcon icon={faJs} title="JavaScript" />
+                    <FontAwesomeIcon icon={faJs} title="JavaScript" onMouseEnter={() => this.highlightSkill("JavaScript")} />
                   </div>
                   <div className="col">
-                    <FontAwesomeIcon icon={faPython} title="Python" />
+                    <FontAwesomeIcon icon={faPython} title="Python" onMouseEnter={() => this.highlightSkill("Python")} />
                   </div>
                   <div className="col">
-                    <FontAwesomeIcon icon={faJava} title="Java" />
+                    <FontAwesomeIcon icon={faJava} title="Java" onMouseEnter={() => this.highlightSkill("Java")} />
                   </div>
                 </div>
                 <div className="row align-items-center">
                   <div className="col">
-                    <FontAwesomeIcon icon={faNode} title="NodeJS" />
+                    <FontAwesomeIcon icon={faGit} title="Git" onMouseEnter={() => this.highlightSkill("Git")} />
                   </div>
                   <div className="col">
-                    <FontAwesomeIcon icon={faHtml5} title="HTML5" />
+                    <FontAwesomeIcon icon={faHtml5} title="HTML5" onMouseEnter={() => this.highlightSkill("HTML5")} />
                   </div>
                   <div className="col">
-                    <FontAwesomeIcon icon={faCss3} title="CSS3" />
+                    <FontAwesomeIcon icon={faCss3} title="CSS3" onMouseEnter={() => this.highlightSkill("CSS3")} />
                   </div>
                 </div>
                 <div className="row align-items-center">
                   <div className="col">
-                    <FontAwesomeIcon icon={faAndroid} title="Android" />
+                    <FontAwesomeIcon icon={faAngular} title="faAngular" onMouseEnter={() => this.highlightSkill("Angular")} />
                   </div>
                   <div className="col">
-                    <FontAwesomeIcon icon={faGit} title="Git" />
+                    <FontAwesomeIcon icon={faAndroid} title="Android" onMouseEnter={() => this.highlightSkill("Android")} />
                   </div>
                   <div className="col">
-                    <FontAwesomeIcon icon={faDatabase} title="SQL" />
+                    <FontAwesomeIcon icon={faNode} title="NodeJS" onMouseEnter={() => this.highlightSkill("NodeJS")} />
                   </div>
                 </div>
               </div>
