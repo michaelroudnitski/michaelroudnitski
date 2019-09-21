55dcdbb584923990e06da48dc2785de36206f0d8
Michael <michaelroudnitski@gmail.com>
2019-09-21 11:28:13 -0400

    modified import in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import style from "./index.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class Index extends React.Component {
   render() {
@@ -22,11 +22,8 @@ class Index extends React.Component {
           </main>
         </div>
       </Router>
-    )
+    );
   }
 }
 
-ReactDOM.render(
-  <Index />,
-  document.getElementById("index")
-);
+ReactDOM.render(<Index />, document.getElementById("index"));
